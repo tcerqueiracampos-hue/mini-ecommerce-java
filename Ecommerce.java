3821f2fda00cc76c56231bf240a69de36e11525f
Thalyta C. Campos <tcerqueiracampos@gmail.com>
2026-09-01 22:41:32 -0300

    Refactor Ecommerce class to manage products

--- Ecommerce.java
+++ Ecommerce.java
@@ -3,21 +3,27 @@ import java.util.Map;
 import javax.swing.JOptionPane;
 
 public class Ecommerce {
-    public static void main(String[] args){
-        
-        Map<Integer, Produto> produtos = new HashMap<>();
-// Adicionar produtos ao mapa
-        Produto notebook = new Produto(101, "Notebook", 3500.00, 5);
-        produtos.put(notebook.getID(), notebook);
+    private Map<Integer, Produto> produtos = new HashMap<>();
+    public void cadastrarProduto() {
 
-        Produto mouse = new Produto(102, "Mouse", 50.00, 20);
-        produtos.put(mouse.getID(), mouse);
+    int id = Integer.parseInt(
+    JOptionPane.showInputDialog("Digite o ID:"));
 
-        Produto teclado = new Produto(103, "Teclado", 150.00, 15);
-        produtos.put(teclado.getID(), teclado);
+    String nome = JOptionPane.showInputDialog("Digite o nome do produto:");
 
-// Apresentar informações de um produto
-        int id = Integer.parseInt(
+    double preco = Double.parseDouble(
+        JOptionPane.showInputDialog("Digite o preço do produto:")
+    );
+    int estoque = Integer.parseInt(
+        JOptionPane.showInputDialog("Digite o estoque do produto:")
+    );
+     Produto produto = new Produto(id, nome, preco, estoque);
+     produtos.put(id, produto);
+     JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso.");
+}
+
+public void buscarProduto() {
+     int id = Integer.parseInt(
         JOptionPane.showInputDialog("Digite o ID do produto:")
         );
         
@@ -28,12 +34,15 @@ public class Ecommerce {
         } else {
         JOptionPane.showMessageDialog(null, "Produto não encontrado.");
         }
-
-        for (Produto produto : produtos.values()) {
+}
+public void listarProdutos() {
+    for (Produto produto : produtos.values()) {
             produto.apresentaProduto();
         }
-// Modificar o preço de um produto
-        double novoPreco = Double.parseDouble(
+}
+
+public void alterarPreco() {
+    double novoPreco = Double.parseDouble(
         JOptionPane.showInputDialog("Digite o novo preço:")
         );
         
@@ -49,8 +58,10 @@ public class Ecommerce {
         } else {
             JOptionPane.showMessageDialog(null, "Produto não encontrado.");
         }
-// Modificar o estoque de um produto
-        int id = Integer.parseInt(
+}
+
+public void alterarEstoque() {
+    int id = Integer.parseInt(
         JOptionPane.showInputDialog(
             "Digite o ID do produto para modificar o estoque:")
         );
@@ -67,16 +78,10 @@ public class Ecommerce {
         } else {
             JOptionPane.showMessageDialog(null, "Produto não encontrado.");
         }
-// Calcular o valor total do estoque
-        double valorTotal = 0;
-        for (Produto produto : produtos.values()) {
-            valorTotal += produto.getPreco() * produto.getEstoque();
-        }
-        JOptionPane.showMessageDialog(null,
-        "Valor total do estoque: R$ " + valorTotal);
+}
 
-// deletar um produto do mapa
-       int id = Integer.parseInt(
+public void removerProduto() {
+    int id = Integer.parseInt(
         JOptionPane.showInputDialog("digite o ID do produto para deletar:")
        );
 
@@ -86,5 +91,64 @@ public class Ecommerce {
         } else {
             JOptionPane.showMessageDialog(null, "Produto não encontrado.");
         }
+}
+  
+public void calcularValorEstoque() {
+    double valorTotal = 0;
+        for (Produto produto : produtos.values()) {
+            valorTotal += produto.getPreco() * produto.getEstoque();
+        }
+        JOptionPane.showMessageDialog(null,
+        "Valor total do estoque: R$ " + valorTotal);
+}
+    public static void main(String[] args){
+
+         Ecommerce ecommerce = new Ecommerce();
+
+        int opcao;
+        do{
+            opcao = Integer.parseInt(
+    JOptionPane.showInputDialog("===== MINI E-COMMERCE =====\n" +
+
+            "1 - Cadastrar produto\n" +
+           "2 - Buscar produto\n" +
+           " 3 - Listar produtos\n" +
+           "4 - Alterar preço\n" +
+            "5 - Alterar estoque\n" +
+            "6 - Remover produto\n" +
+            "7 - Calcular valor do estoque\n" +
+            "0 - Sair\n\n" +
+
+            "Digite uma opção:"));
+            switch(opcao){
+                case 1:
+                    ecommerce.cadastrarProduto();
+                    break;
+                case 2:
+                    ecommerce.buscarProduto();
+                    break;
+                case 3:
+                    ecommerce.listarProdutos();
+                    break;
+                case 4:
+                    ecommerce.alterarPreco();
+                    break;
+                case 5:
+                    ecommerce.alterarEstoque();
+                    break;
+                case 6:
+                    ecommerce.removerProduto();
+                    break;
+                case 7:
+                    ecommerce.calcularValorEstoque();
+                    break;
+                case 0:
+                    JOptionPane.showMessageDialog(null, "Saindo do programa.");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida. Tente novamente.");
+            }
+        }
+        while(opcao != 0);
        }
     }
